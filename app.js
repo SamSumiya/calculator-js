aae56790353b56bfc615b6bdf93f050fb25b66a0
chenerychen <60025491+chenerychen@users.noreply.github.com>
2022-01-31 20:59:09 -0800

    mathmatical operations work in console

--- app.js
+++ app.js
@@ -1,38 +1,67 @@
-let currentRunningSum = 0 
-let buffer = '0'
-let previousSymbol; 
+let currentRunningSum = 0;
+let buffer = '0';
+let previousSymbol;
 
-const screen = document.querySelector('.screen')
-
+const screen = document.querySelector('.screen');
 
 function buttonClick(clickedButton) {
   if (isNaN(parseInt(clickedButton))) {
-    handleSymbol(clickedButton)
+    handleSymbol(clickedButton);
   } else {
-    handleNumber(clickedButton)
+    handleBufferNumber(clickedButton);
   }
   screen.innerText = buffer;
 }
 
 function handleSymbol(symbol) {
   switch (symbol) {
-    // case 'C':
+    case 'C':
+      (buffer = '0'), (currentRunningSum = 0);
+      break;
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+      handleMath(symbol);
+      break;
+  }
+}
+
+function handleMath(symbol) {
+  if (currentRunningSum === '0') return;
+
+  const intBuffer = +buffer;
+  if (intBuffer === 0) {
+    currentRunningSum = intBuffer;
+  } else {
+    doMathFn(intBuffer);
+  }
+  previousSymbol = symbol;
+  buffer = 0;
+}
+
+function doMathFn(number) {
+  if (previousSymbol === '+') {
+    currentRunningSum += number;
+  } else if (previousSymbol === '-') {
+    currentRunningSum -= number;
+  } else if (previousSymbol === '*') {
+    currentRunningSum *= number;
+  } else if (previousSymbol === '/') {
+    currentRunningSum /= number;
   }
 }
 
 
-function handleNumber(number) {
-  buffer === '0' ? buffer = number : buffer += number
- 
-  screen.innerText = buffer
+
+function handleBufferNumber(number) {
+  buffer === '0' ? (buffer = number) : (buffer += number);
 }
 
 function init() {
-  document
-    .querySelector('.calc-buttons')
-    .addEventListener('click', (event) => {
-     buttonClick(event.target.innerText)
-    })
+  document.querySelector('.calc-buttons').addEventListener('click', (event) => {
+    buttonClick(event.target.innerText);
+  });
 }
 
-init()
+init();
