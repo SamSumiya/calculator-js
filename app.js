023905d0ddd130549d6e2814902063f9c24b0cfb
chenerychen <60025491+chenerychen@users.noreply.github.com>
2022-01-31 20:37:52 -0800

    added button click to get the DOM selected dom element

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-const screen = document.querySelector('screen')
+let currentRunningSum = 0 
+let buffer = '0'
+let previousSymbol; 
+
+const screen = document.querySelector('.screen')
+
+
+function buttonClick(clickedButton) {
+  if (isNaN(parseInt(clickedButton))) {
+    handleSymbol(clickedButton)
+  } else {
+    handleNumber(clickedButton)
+  }
+  screen.innerText = buffer;
+}
+
+function handleSymbol(symbol) {
+  switch (symbol) {
+    // case 'C':
+  }
+}
+
+
+function handleNumber(number) {
+  buffer === '0' ? buffer = number : buffer += number
+ 
+  screen.innerText = buffer
+}
+
+function init() {
+  document
+    .querySelector('.calc-buttons')
+    .addEventListener('click', (event) => {
+     buttonClick(event.target.innerText)
+    })
+}
+
+init()
